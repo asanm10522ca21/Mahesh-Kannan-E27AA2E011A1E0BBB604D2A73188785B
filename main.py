2864512f18e5bbea31dbb1eabf2b52a9308a8b79
asanm10522ca21 <144436416+asanm10522ca21@users.noreply.github.com>
2023-09-27 10:54:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-# 1.2 Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-"""
-year % 40 &
-year % 100 !=0 /
-year% 400 == 0
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive value.")
 
-"""
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
-    return True
-  else:
-    return False
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
 
-year = int(input("Enter a year :"))
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
 
-if isLeapYear(year):
-  print('() is a Leap Year.'.format(year))
-else:
-  print('() is not a Leap Year.'.format(year))
+
+# Create an instance of the BankAccount class
+account1 = BankAccount("123456789", "John Doe", 1000.0)
+
+# Test deposit and withdrawal functionality
+account1.display_balance()
+account1.deposit(500.0)
+account1.withdraw(200.0)
+account1.display_balance()
